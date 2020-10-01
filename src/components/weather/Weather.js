4d3d5fc6607a140c9133cb4b5474f824a710b06a
Hazem-Kamel <hazem.portofolio@gmail.com>
2020-10-01 18:37:54 +0200

    Showing five days for Salzburg weather data

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import bootstrap
 // import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './Weather.css'
 // import Weather icons
 import 'weather-icons/css/weather-icons.css';
 
@@ -12,54 +12,59 @@ class Weather extends React.Component{
         this.state={
           city:'salzburg',
           country:'Austria',
-          icon:undefined,
-          main:undefined,
-          celsius:undefined,
-          temp_max:undefined,
-          temp_min:undefined,
-          description:'',
-          wind:undefined,
-          error:false
+          icon:[],
+          main:null,
+          celsius:null,
+          temp_max:null,
+          temp_min:null,
+          description:null,
+          wind:null,
+          error:false,
+          fiveWeatherData:[],
+          displayData:[]
         }
       }
 
-componentDidMount(){
+componentWillMount(){
     this.getWeather();
 }
       getWeather = async()=>{
-        // e.preventDefault();
-                    //Api key
 const Api_key='8cc1f7a80f278edcf892761e33af1953'
           const {city , country } =this.state
-        const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}
-        &appid=${Api_key}`
-        , { method: 'GET',
+        const api_call=await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city} &appid=${Api_key}`,
+         { method: 'GET',
         mode: 'cors',
         header:{
           'Access-Control-Allow-Origin': '*'
         }
         })
       const response=await api_call.json();
-    //   console.log(response.list)
-
-      this.setState({
-        city:`${response.name},${response.sys.country}`,
-        country:response.sys.country,
-        celsius:this.celsiusConvert(response.main.temp),
-        temp_max: this.celsiusConvert(response.main.temp_max),
-        temp_min: this.celsiusConvert(response.main.temp_min),
-        description:response.weather[0].description,
-        icon:response.weather[0].icon,
-        wind:response.wind.speed
-      })
-      this.get_WeatherIcon(this.weatherIcon,response.weather[0].id)
+        this.state.fiveWeatherData.push(response.list[0])
+    response.list.map(forecast => {
+        if(forecast.dt_txt.split(' ')[0] !== response.list[0].dt_txt.split(' ')[0] && forecast.dt_txt.split(' ')[1] === response.list[0].dt_txt.split(' ')[1] ) {
+            this.state.fiveWeatherData.push(forecast)
+        }
+    })
+    let temp=[]
+    this.state.fiveWeatherData.map(day => {
+        // this.state.displayData.push
+        temp.push
+        ({
+            "celsius":this.celsiusConvert(day.main.temp),
+            "wind":day.wind.speed,
+            "desc":day.weather[0].description,
+            "max":this.celsiusConvert(day.main.temp_max),
+            "min":this.celsiusConvert(day.main.temp_min),
+        })
+          this.get_WeatherIcon(this.weatherIcon,day.weather[0].id)
+    })
+     this.setState({displayData:temp})
           }
           celsiusConvert(temp){
             let celsius=Math.floor(temp-273.15)
             return celsius
           }
           weatherIcon={
-        
             Thunderstorm:'wi-thunderstorm',
             Drizzle:'wi-sleet',
             Rain:'wi-storm-showers',
@@ -67,50 +72,55 @@ const Api_key='8cc1f7a80f278edcf892761e33af1953'
             Atmosphere:'wi-fog',
             Clear:'wi-day-sunny',
             Clouds:'wi-day-fog'
-        
-        
           }
-        
           get_WeatherIcon(icons,rangeID){
             switch(true){
               case rangeID>=200 && rangeID<=232:
-                this.setState({icon:this.weatherIcon.Thunderstorm})
+                this.state.icon.push(this.weatherIcon.Thunderstorm)
                 break;
                 case rangeID>=300 && rangeID<=321:
-                this.setState({icon:this.weatherIcon.Drizzle})
+                this.state.icon.push(this.weatherIcon.Drizzle)
                 break;
                 case rangeID>=500 && rangeID<=531:
-                this.setState({icon:this.weatherIcon.Rain})
+                this.state.icon.push(this.weatherIcon.Rain)
                 break;
                 case rangeID>=600 && rangeID<=622:
-                this.setState({icon:this.weatherIcon.Snow})
+                this.state.icon.push(this.weatherIcon.Snow)
                 break;
                 case rangeID>=701 && rangeID<=781:
-                this.setState({icon:this.weatherIcon.Atmosphere})
+                this.state.icon.push(this.weatherIcon.Atmosphere)
                 break;
                 case rangeID === 800:
-                this.setState({icon:this.weatherIcon.Clear})
+                this.state.icon.push(this.weatherIcon.Clear)
                 break;
                 case rangeID>=801 && rangeID<=804:
-                this.setState({icon:this.weatherIcon.Clouds})
+                this.state.icon.push(this.weatherIcon.Clouds)
                 break;
                 default:
-                  this.setState({icon:this.weatherIcon.Clouds})
-        
+                    this.state.icon.push(this.weatherIcon.Clouds)
+                //   this.setState({icon:this.weatherIcon.Clouds})
             }
-            
           }
-      
       render(){
-          const {celsius,description,wind,icon} = this.state
+          const {celsius,description,wind,icon,displayData} = this.state
           return (
           <div>
-        <i className={`wi ${icon} display-1`}></i>
-        <h4>{description}</h4>
-        <h3>{celsius}&deg; </h3>
-        <h3>{wind} m/s </h3>
-
-          </div>)
+              {
+                console.log(displayData,displayData.length)
+              }
+              {
+              displayData.map((day,index) => 
+           (
+                    <div className="card">
+                         <i className={`wi ${icon[index]} display-1`}></i> 
+                         <h4>{day.desc}</h4>
+                         <h3>{day.celsius}&deg; </h3>
+                         <h3>{day.wind} m/s </h3>
+                    </div>
+                      
+              ))}
+          </div>
+          )
       }
 }
 
